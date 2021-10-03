acb71fde6a72842cb61897a2a913c7123b1ba46a
정현우 <wijeong@gmail.com>
2021-10-03 16:47:33 +0900

    delete db and fix Readme

--- server/controllers/getTimeSlots.js
+++ server/controllers/getTimeSlots.js
@@ -1,15 +1,12 @@
-const { requestbody } = require("../models/requestbody");
-
 module.exports = async (req, res) => {
   try {
-    const RequestParse = JSON.parse(requestbody);
-    // const {
-    //   start_day_identifier,
-    //   timezone_identifier,
-    //   service_duration,
-    //   days,
-    //   timeslot_interval,
-    // } = req.body;
+    const {
+      start_day_identifier,
+      timezone_identifier,
+      service_duration,
+      days,
+      timeslot_interval,
+    } = req.body;
   } catch (err) {
     console.error(err);
     res.status(400).json({ message: err });
